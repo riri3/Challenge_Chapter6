698ccd6391de7ec8b9242a816f54f13234541588
riri3 <kamijooor@gmail.com>
2021-06-07 15:08:36 +0000

    [update]follow/follower

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,16 @@ class User < ApplicationRecord
 
   has_many :books, dependent: :destroy
   has_many :book_comments, dependent: :destroy
-  has_many :favorites, dependent: :destroy
+  has_many :favorites, dependent: :
+
+  # 自分がフォローする（与フォロー）側の関係性
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  # 自分がフォローされる（被フォロー）側の関係性
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
-  has_many :followings, through: :relationships, source: :followed # 自分がフォローしている人
-  has_many :followers, through: :reverse_of_relationships, source: :follower # 自分をフォローしている人
+  # 与フォロー関係を通じて参照→自分がフォローしている人
+  has_many :followings, through: :relationships, source: :followed
+  # 被フォロー関係を通じて参照→自分をフォローしている人
+  has_many :followers, through: :reverse_of_relationships, source: :follower
 
 
   def follow(user_id)
